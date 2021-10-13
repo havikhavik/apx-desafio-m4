92efcd01ca4cbc29a867ad881416439c451b7179
Lucas <pascual_lucas117@hotmail.com>
2021-10-13 03:37:01 -0300

    add http post method

--- contact/contact.js
+++ contact/contact.js
@@ -3,7 +3,7 @@ function contactLoad(el) {
 
   componentEl.innerHTML = `     <div class="custom-contact">
   <h2 class="custom-contact__title">Escribime</h2>
-  <form action="" class="custom-contact__form">
+  <form action="submit" class="custom-contact__form">
     <label for="name" class="custom-contact__name-label">NOMBRE</label>
     <input type="text" id="name" class="custom-contact__name-input" />
     <label for="email" class="custom-contact__email-label">EMAIL</label>
@@ -21,6 +21,34 @@ function contactLoad(el) {
     <button class="custom-contact__btn">Enviar</button>
   </form>
 </div>`;
+
+  const formEl = componentEl.querySelector(".custom-contact__form");
+
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const $name = componentEl.querySelector("#name").value;
+    const $email = componentEl.querySelector("#email").value;
+    const $message = componentEl.querySelector("#textarea").value;
+
+    const url = "https://apx-api.vercel.app/api/utils/dwf";
+
+    fetch(url, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        name: $name,
+        to: $email,
+        message: $message,
+      }),
+    })
+      .then(() => {
+        alert("Gracias por enviar tu mensaje!");
+      })
+      .catch(() => {
+        alert("error al enviar el mensaje");
+      });
+  });
 
   el.insertBefore(componentEl, el.firstChild);
 }
